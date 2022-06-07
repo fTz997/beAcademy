7f5859b40241773ca0dd528e9e29efe03ac126ad
Victor Feitoza <84550540+fTz997@users.noreply.github.com>
2022-06-07 17:43:01 -0300

    a

---
+++ beacademy-devstart-php8/modulo2/encapsulamento/Produto.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+class Produto 
+{
+  private string $nome;
+  private float $valor;
+
+
+  public function getNome():string 
+  {
+    return $this->nome;
+  }
+  public function setNome(string $nome):void 
+  {
+    $this->nome = $nome;
+  }
+
+  public function getValor():float 
+  {
+    return $this->valor;
+  }
+  public function setValor(float $valor):void 
+  {
+    $this->valor = $valor;
+  }
+}
